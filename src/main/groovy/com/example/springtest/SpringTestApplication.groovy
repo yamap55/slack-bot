19e7e154144097efa65d5a712b3cc7470a1f6784
yamap55 <tetuya120@hotmail.com>
2017-11-02 17:59:12 +0900

    [add] spring boot application

---
+++ src/main/groovy/com/example/springtest/SpringTestApplication.groovy
@@ -0,0 +1,12 @@
+package com.example.springtest
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringTestApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SpringTestApplication, args
+	}
+}
