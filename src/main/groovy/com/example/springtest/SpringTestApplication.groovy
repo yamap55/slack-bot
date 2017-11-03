36d39779a9418734c0d1119d7d7922f59362c9ac
yamap55 <tetuya120@hotmail.com>
2017-11-04 00:20:45 +0900

    [add] component and config

--- src/main/groovy/com/example/springtest/SpringTestApplication.groovy
+++ src/main/groovy/com/example/springtest/SpringTestApplication.groovy
@@ -1,12 +1,22 @@
 package com.example.springtest
 
+import com.example.springtest.app.ComponentTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class SpringTestApplication {
+class SpringTestApplication implements CommandLineRunner {
 
+	@Autowired
+	ComponentTest componentTest;
 	static void main(String[] args) {
 		SpringApplication.run SpringTestApplication, args
+	}
+
+	@Override
+	void run(String... args) throws Exception {
+		componentTest.hoge()
 	}
 }
---
+++ src/main/groovy/com/example/springtest/app/ComponentTest.groovy
@@ -0,0 +1,15 @@
+package com.example.springtest.app
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+class ComponentTest {
+    @Value('${test.value:aaa}')
+    def str
+
+    def hoge() {
+        println "hoge"
+        println str
+    }
+}
