5666b9cf2bd1e36c131757c7f0d04ce93b2b609a
yamap55 <tetuya120@hotmail.com>
2017-11-07 23:37:10 +0900

    [add] image edit class

--- src/main/groovy/com/example/slackbot/SlackBotApplication.groovy
+++ src/main/groovy/com/example/slackbot/SlackBotApplication.groovy
@@ -1,6 +1,7 @@
 package com.example.slackbot
 
 import com.example.slackbot.app.Gyazo
+import com.example.slackbot.app.ImageEditor
 import com.example.slackbot.app.SelenideExecuter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
@@ -16,6 +17,9 @@ class SlackBotApplication implements CommandLineRunner {
 	@Autowired
 	SelenideExecuter selenide
 
+	@Autowired
+	ImageEditor imageEditor
+
 	static void main(String[] args) {
 		SpringApplication.run SlackBotApplication, args
 	}
@@ -23,6 +27,9 @@ class SlackBotApplication implements CommandLineRunner {
 	@Override
 	void run(String... args) throws Exception {
 		def capInfo = selenide.getCapture("https://weather.yahoo.co.jp/weather/13/4410.html", [".forecastCity"])
-		println gyazo.upload(capInfo.file)
+
+		// Macでは何故か値を2倍する必要あり
+		def f = imageEditor.subImage(capInfo.file, capInfo.x * 2, capInfo.y * 2, capInfo.width * 2, capInfo.height * 2)
+		println gyazo.upload(f)
 	}
 }
